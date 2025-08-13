bc40859d22f0ed67b51bc695d27697bbc2e8e948
XD <xdbreyg@gmail.com>
2025-08-13 17:00:58 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -1,75 +1,104 @@
 import streamlit as st
 import sqlite3 as sq
+import time as t
 
 
-
-
-conn = sq.connect('Banco_de_dados.db')
+conn = sq.connect('Banco_de_dados.db', check_same_thread=False)
 cursor = conn.cursor()
+
     
 def verify_user(user, password):
-    cursor.execute('select * from Usuarios')
-    result = cursor.fetchall()
-    if user in result[0][1] and password in result[0][2]:
-        return True
-
-def verify_password(self, password):
-    if self.password == True:
-        return True
+    cursor.execute('select * from professores')
+    professores = cursor.fetchall()
+    for i in range(len(professores)):
+        if user in professores[i][1] and password in professores[i][2]:
+            return 'professor'
+        else:
+            pass
+    cursor.execute('select * from alunos')
+    alunos = cursor.fetchall()
+    password = hash(password)
+    for i in range(len(alunos)):
+        if user in alunos[i][1] and password in alunos[i][3]:
+            return 'aluno'
+        else:
+            return False
     
-def main():
-    # Título do app
-    st.title("Painel Administrador")
-
-    # Barra lateral para navegação
-    menu = st.sidebar.selectbox(
-        "Escolha um menu",
-        ("Administrador", "Usuário")
-    )
-
     # Exibindo conteúdo com base na seleção
-    if menu == "Administrador":
-        exibir_menu_administrador()
-    elif menu == "Usuário":
-        exibir_menu_usuario()
+    
         
 @st.dialog('dialogo criar usuario')
 def create_user():
     st.title('Criar cadastro')
-    st.text_input('Insira o nome do aluno')
-    
+    nome = st.text_input('Insira o nome do aluno')
+    cpf = st.text_input('cpf o nome do aluno')
+    senha = hash(st.text_input('Insira a senha do aluno'))
+    if st.button('confirmar'):
+        cursor.execute('INSERT INTO alunos (nome, cpf, senha) VALUES (?, ?, ?)', (nome, cpf, senha))
+        conn.commit()
+        
         
 @st.dialog('dialogo inicial')
 def dialog_adm():
     st.subheader("""seja bem vindo""")
     if st.button('Cadastrar aluno'):
-        pass
+        create_user()
     st.button('listar usuarios')
     st.button('remover usuarios')
     
     
 # Função para o conteúdo do menu de Administrador
-def exibir_menu_administrador():
-    usuarioadm = st.text_input("Usuario")
-    senhaadm = st.text_input("Senha")
-    if st.button("""Confirmar
-    """):
-        request = verify_user(usuarioadm, senhaadm)
-        if request == True:
-            dialog_adm()
+def exibir_menu_login():
+    with st.form(key='login-form'):
+        st.title("Menu de login")
+        usuarioadm = st.text_input("Usuario")
+        senhaadm = st.text_input("Senha")
+        if st.form_submit_button("""Confirmar"""):
+            request = verify_user(usuarioadm, senhaadm)
+            if request == 'professor':
+                    st.session_state.key = 'professor'
+                    st.rerun()
+            elif request == 'aluno':
+                    st.session_state.key = 'aluno'
+                    st.rerun()
                 
+            else: 
+                st.warning('Usuario ou senha incorretos')
+                
+def menu_alunos():
+    st.title('Seja bem vindo ao menu de alunos')
     # Aqui você pode adicionar mais funcionalidades conforme necessário.
 
 # Função para o conteúdo do menu de Usuário
-def exibir_menu_usuario():
-    st.subheader("Bem-vindo ao menu de Usuário")
-    st.write("""
-        No menu de usuário, você pode:
-        - Visualizar seus dados
-        - Alterar configurações pessoais
-        - Acessar suas funcionalidades
-    """)
+def exibir_menu_professor():
+    st.title("Seja bem vindo")
+    st.subheader('Listar alunos')
+    listbutton = st.button('confirmar', key='list')
+    st.subheader('Remover alunos')
+    removebutton = st.button('confirmar', key='removebutton')
+    st.subheader('Cadastrar alunos')
+    registerbutton = st.button('Confirmar', key='registerbutton')
+    if registerbutton:
+        create_user()
+    st.subheader('Sair')
+    exitbutton = st.button('confirmar', key='exitbutton')
+    if exitbutton:
+        st.session_state['key'] = None
+        st.rerun()
     # Aqui você pode adicionar mais funcionalidades conforme necessário.
+
+def main():
+    # Título do app
+    if 'key' not in st.session_state:
+        st.session_state.key = 'indefinido'
+        exibir_menu_login()
+
+    elif st.session_state.key == 'professor':
+        exibir_menu_professor()
+        
+    elif st.session_state.key == 'aluno':
+        menu_alunos()
+
 
 # Execução do aplicativo
 if __name__ == "__main__":
